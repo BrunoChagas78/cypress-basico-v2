10ce3a624e899d7a46f6454572fa47b59849e7f6
Bruno Chagas <brunoccchagas@hotmail.com>
2023-10-11 14:04:46 -0300

    creating cypress project

---
+++ cypress/integration/CAC-TAT.spec.js
@@ -0,0 +1,22 @@
+/// <reference types="Cypress" />
+
+describe('Central de Atendimento ao Cliente TAT', function() {
+        beforeEach(function() {
+        cy.visit('./src/index.html')
+    })
+
+    it('verifica o título da aplicação', function() {
+        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+    })
+
+    it('preenche os campos obrigatórios e envia o formulário', function() {
+        const longText = 'olá, olá, olá, olá k,olá,olá,olá ,olá, olá,olá, olá, olá, olá k,olá,olá,olá ,olá, olá,oláoláolá, olá, olá, olá k,olá,olá,olá ,olá, olá,oláolá'
+        cy.get('#firstName').type('Bruno')
+        cy.get('#lastName').type('Chagas')
+        cy.get('#email').type('brunoccchagas@hotmail.com')
+        cy.get('#open-text-area').type(longText, {delay : 0})
+        cy.get('button[type="submit"]').click()
+        cy.get('.success').should('be.visible')
+    })
+    
+  })
